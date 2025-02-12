aec165429f75bc06eb0bb8290e007e1cd3c80790
Aswin <aswinksatheesh07@gmail.com>
2025-02-12 20:06:59 +0530

    loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserForm from "./pages/UserForm";
+import { LoginForm } from "./components/login-form";
+
 
 function App() {
   return (
+    
     <Router>
       <div>
-        <h1>Hospital Management System</h1>
-        <Routes>
-          <Route path="/" element={<UserForm />} />
-        </Routes>
+      <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
       </div>
+    </div>
+    
+        <Routes>
+        
+        
+        </Routes>
+     </div> 
     </Router>
+
   );
 }
 
